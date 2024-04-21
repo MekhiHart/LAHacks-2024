5de2538656a70c17a1269a4f598280dedc0b043c
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-04-21 00:11:18 -0700

    made font smaller for date ingredients expired for IngredientComponent

--- client/src/components/IngredientComponent/IngredientComponent.tsx
+++ client/src/components/IngredientComponent/IngredientComponent.tsx
@@ -12,10 +12,9 @@ interface IngredientComponentProps{
 export default function IngredientComponent(props:IngredientComponentProps){
     const {name, quantity, expirationDate, weight} = props.IngredientComponentData
     return(
-        <div>
-            {quantity && <span>({quantity}) {name}</span>}
-            {weight && <span>({weight}) {name}</span>}
-            <span>{new Date(expirationDate).toDateString()}</span>
+        <div style={{margin: "-5px 0px 10px 0px"}}>
+            {quantity && <h3>({quantity}) {name}</h3>}
+            <h4 style={{fontSize:"12px", margin:"-10px 0px"}}>{new Date(expirationDate).toDateString()}</h4>
         </div>
     )
 
