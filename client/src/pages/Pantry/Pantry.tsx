9d71b82081e5a675f2b209dbaf1527ea4d4aa0f2
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-04-20 17:28:08 -0700

    added navigation in 'add ingredients' button

--- client/src/pages/Pantry/Pantry.tsx
+++ client/src/pages/Pantry/Pantry.tsx
@@ -1,10 +1,13 @@
 import styles from './Pantry.module.css'
 
+import { useNavigate } from 'react-router-dom';
+
 const Pantry = () =>{
+    const navigate = useNavigate()
     return(
         <div className={styles.container}>
         <h2 className={styles.header}>Pantry is empty</h2>
-        <button className={styles.addButton}>Add ingredients</button>
+        <button className={styles.addButton} onClick={() => navigate("scan")}>Add ingredients</button>
         </div>
     )
 }
